6defc8acad41fa2dfaa30028c7e1c6f1a8551bd3
Gabriel Naiman <gabynaiman@gmail.com>
2015-01-21 14:46:02 -0300

    Added test for set

--- lib/restruct/set.rb
+++ lib/restruct/set.rb
@@ -2,40 +2,108 @@ module Restruct
   class Set < Structure
 
     include Enumerable
+    extend Forwardable
+
+    def_delegators :to_set, :union, :|, :+, 
+                            :intersection, :&, 
+                            :difference, :-,
+                            :proper_subset?, :<,
+                            :subset?, :<=,
+                            :proper_superset?, :>,
+                            :superset?, :>=,
+                            :intersect?, :disjoint?,
+                            :^
 
     def add(member)
-      redis.call 'SADD', id, member
+      _add member
+      self
     end
     alias_method :<<, :add
 
+    def add?(member)
+      _add(member) == 0 ? nil : self
+    end
+
+    def merge(members)
+      _add *members
+      self
+    end
+
     def delete(member)
-      redis.call 'SREM', id, member
+      _delete member
+      self
+    end
+
+    def delete?(member)
+      _delete(member) == 0 ? nil : self
+    end
+
+    def subtract(members)
+      _delete *members
+      self
+    end
+
+    def delete_if
+      each { |e| delete e if yield e }
+      self
+    end
+
+    def keep_if
+      each { |e| delete e unless yield e }
+      self
+    end
+    alias_method :select!, :keep_if
+
+    def clear
+      destroy
+      self
+    end
+
+    def size
+      redis.call 'SCARD', id
+    end
+    alias_method :count, :size
+    alias_method :length, :size
+
+    def empty?
+      size == 0
+    end
+
+    def include?(member)
+      redis.call('SISMEMBER', id, serialize(member)) == 1
     end
 
     def each(&block)
       to_a.each(&block)
     end
 
-    def empty?
-      redis.call('EXISTS', id) == 0
-    end
-
-    def count
-      redis.call 'SCARD', id
-    end
-
-    def include?(member)
-      redis.call('SISMEMBER', id, member) == 1
-    end
-
     def to_a
-      redis.call 'SMEMBERS', id
+      redis.call('SMEMBERS', id).map { |e| deserialize e }
     end
-    alias_method :to_primitive, :to_a
 
     def to_set
       to_a.to_set
     end
+    alias_method :to_primitive, :to_set
     
+    private
+
+    def _add(*members)
+      redis.call 'SADD', id, *members.map { |m| serialize m }
+    end
+
+    def _delete(*members)
+      redis.call 'SREM', id, *members.map { |m| serialize m }
+    end
+
+
+    def serialize(string)
+      string
+    end
+
+    def deserialize(string)
+      string
+    end
+
   end
 end
---
+++ spec/set_spec.rb
@@ -0,0 +1,256 @@
+require 'minitest_helper'
+
+[Restruct::Set].each do |klass|
+
+  describe klass do
+
+    let(:set) { klass.new }
+
+    def fill(elements)
+      redis.call 'SADD', set.id, *(elements.map { |e| set.send(:serialize, e) })
+    end
+
+    describe 'Setters' do
+      
+      %w(add <<).each do |method|
+        it method do
+          fill %w(a b)
+          
+          set.send(method, 'b').must_equal set
+          set.to_set.must_equal Set.new %w(a b)
+
+          set.send(method, 'c').must_equal set
+          set.to_set.must_equal Set.new %w(a b c)
+        end
+      end
+      
+      it 'add?' do
+        fill %w(a b)
+        
+        set.add?('b').must_be_nil
+        set.to_set.must_equal Set.new %w(a b)
+
+        set.add?('c').must_equal set
+        set.to_set.must_equal Set.new %w(a b c)
+      end
+
+      it 'merge' do
+        fill %w(a b)
+
+        set.merge(%w(b c d)).must_equal set
+        set.to_set.must_equal Set.new %w(a b c d)
+      end
+      
+      it 'delete' do
+        fill %w(a b c d)
+
+        set.delete('x').must_equal set
+        set.to_set.must_equal Set.new %w(a b c d)
+
+        set.delete('b').must_equal set
+        set.to_set.must_equal Set.new %w(a c d)
+      end
+      
+      it 'delete?' do
+        fill %w(a b c d)
+
+        set.delete?('x').must_be_nil
+        set.to_set.must_equal Set.new %w(a b c d)
+
+        set.delete?('b').must_equal set
+        set.to_set.must_equal Set.new %w(a c d)
+      end
+      
+      it 'subtract' do
+        fill %w(a b c d)
+
+        set.subtract(%w(b c x)).must_equal set
+        set.to_set.must_equal Set.new %w(a d)
+      end
+
+      it 'delete_if' do
+        fill %w(a b c)
+
+        set.delete_if { |e| e == 'a' }.must_equal set
+        set.to_set.must_equal Set.new(%w(b c))
+      end
+
+      %w(keep_if select!).each do |method|
+        it method do
+          fill %w(a b c)
+
+          set.send(method) { |e| e == 'a' }.must_equal set
+          set.to_set.must_equal Set.new(%w(a))
+        end
+      end
+
+      it 'clear' do
+        fill %w(a b c d)
+
+        set.clear.must_equal set
+        set.to_set.must_be_empty
+      end
+      
+    end
+
+    describe 'Info' do
+
+      %w(size count length).each do |method|
+        it method do
+          fill %w(a b c)
+          set.send(method).must_equal 3
+        end
+      end
+
+      it 'empty?' do
+        set.must_be :empty?
+        fill %w(a b c)
+        set.wont_be :empty?
+      end
+
+      it 'include?' do
+        fill %w(a b c)
+
+        assert set.include? 'a'
+        refute set.include? 'z'
+      end
+
+    end
+
+    describe 'Transformations' do
+
+      it 'to_a' do
+        fill %w(a b c)
+        set.to_a.sort.must_equal %w(a b c)
+      end
+
+      %w(to_set to_primitive).each do |method|
+        it method do
+          fill %w(a b c)
+          set.send(method).must_equal Set.new(%w(a b c))
+        end
+      end
+
+    end
+
+    describe 'Enumerable' do
+
+      it 'included module' do
+        assert klass.included_modules.include? Enumerable
+      end
+
+      it 'each' do
+        fill %w(a b c)
+
+        members = Set.new
+        set.each { |e| members << e }
+
+        members.must_equal set.to_set
+      end
+
+    end
+
+    describe 'Sets' do
+      
+      %w(union | +).each do |method|
+        it method do
+          fill %w(a b c)
+          set.send(method, %w(a x y)).must_equal Set.new(%w(a b c x y))
+        end
+      end
+
+      %w(intersection &).each do |method|
+        it method do
+          fill %w(a b c)
+          set.send(method, %w(b a z)).must_equal Set.new(%w(a b))
+        end
+      end
+      
+      %w(difference -).each do |method|
+        it method do
+          fill %w(a b c)
+          set.send(method, %w(a z)).must_equal Set.new(%w(b c))
+        end
+      end
+
+      %w(proper_subset? <).each do |method|
+        it method do
+          fill %w(a b)
+          
+          assert set.send(method, Set.new(%w(a b c)))
+          refute set.send(method, Set.new(%w(a b)))
+          refute set.send(method, Set.new(%w(a x)))
+        end
+      end
+
+      %w(subset? <=).each do |method|
+        it method do
+          fill %w(a b)
+          
+          assert set.send(method, Set.new(%w(a b c)))
+          assert set.send(method, Set.new(%w(a b)))
+          refute set.send(method, Set.new(%w(a x)))
+        end
+      end
+
+      %w(proper_superset? >).each do |method|
+        it method do
+          fill %w(a b c)
+          
+          assert set.send(method, Set.new(%w(a b)))
+          refute set.send(method, Set.new(%w(a b c)))
+          refute set.send(method, Set.new(%w(a x)))
+        end
+      end
+
+      %w(superset? >=).each do |method|
+        it method do
+          fill %w(a b c)
+          
+          assert set.send(method, Set.new(%w(a b)))
+          assert set.send(method, Set.new(%w(a b c)))
+          refute set.send(method, Set.new(%w(a x)))
+        end
+      end
+      
+      it 'intersect?' do
+        fill %w(a b c)
+
+        assert set.intersect? Set.new(%w(a x y))
+        refute set.intersect? Set.new(%w(x y z))
+      end
+      
+      it 'disjoint?' do
+        fill %w(a b c)
+
+        refute set.disjoint? Set.new(%w(a x y))
+        assert set.disjoint? Set.new(%w(x y z))
+      end
+
+      it '^' do
+        fill %w(a b c)
+        (set ^ %w(a x y)).must_equal Set.new(%w(b c x y))
+      end
+
+    end
+
+    it 'Equality' do
+      copy = klass.new id: set.id
+      assert set == copy
+      assert set.eql? copy
+    end
+
+    it 'Dump/Restore' do
+      fill %w(a b c)
+      
+      dump = set.dump
+      other = klass.new
+      other.restore dump
+
+      other.id.wont_equal set.id
+      other.to_primitive.must_equal set.to_primitive
+    end
+
+  end
+
+end
